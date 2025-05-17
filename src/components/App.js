60e33110048a61d404101e3e432d1db2cb368f4a
Vishnu <bandarivishnu143@gmail.com>
2025-05-17 10:47:58 +0530

    don

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,29 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const[log,setLog]=React.useState(false);
+  const Loggin=({onlogin})=>{
+ 
+ return(
+  <div>
+ <labele>Username:</labele>
+ <input type="text"/>
+ <br></br><br></br>
+ <label>Password:</label>
+ <input type="text"/>
+ <br></br><br></br>
+ <button onClick={onlogin}>login</button>
+ </div>
+ );
+};
+return(
+  <div>
+    {log ?(
+      <p>You are logged in !</p>
+      ):(
+        <Loggin onlogin={()=>setLog(true)}/>
+    )}
+  </div>
+)
 }
-
 export default App
